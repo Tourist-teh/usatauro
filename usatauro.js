fe7b6d17ab7452a8c3fa9ac2f2061511f633f51c
Tourist-teh <alexandre_abramishvili@yahoo.com>
2018-06-06 18:03:46 +0400

    Add files via upload

---
+++ usatauro.js
@@ -0,0 +1,7 @@
+﻿window.onkeydown = function(e){
+	if( e.keyCode == 13 && e.shiftKey ){
+		w=document.getElementsByTagName("textarea")[0].value,ans="";
+		for(var i=0; i<w.split(" ").length; i++){ans+=w.split(" ")[i].split("").reverse().join("")+" ";} document.getElementsByTagName("textarea")[0].value = ans;
+		document.getElementsByName("submit")[0].click();
+	}
+}
